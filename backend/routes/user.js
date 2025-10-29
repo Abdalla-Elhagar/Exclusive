4f59651d693e5cba6c7e4ef1a43e1a465b3640f6
Abdalla Elhagar <nanoelhgar@gmail.com>
2025-10-29 15:20:56 +0300

    handled login process

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,12 @@
 import {Router} from "express";
 import { handleRegister } from "../services/userServices/register.js";
+import { handleLogin } from "../services/userServices/login.js";
 
 const router = Router()
 
 router.post("/register", handleRegister)
+
+router.post("/login", handleLogin)
 
 
 
