b33728dfdc9de675d2409710132e4198d07ac382
Abdalla Elhagar <nanoelhgar@gmail.com>
2025-11-02 18:54:34 +0200

    Backend:
    - created favorite model and router
    
    - handled favorite ptoduct
    
    - handled add product to favorites
    
    - handled remove product from favorites

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ import mongoose from "mongoose";
 import userRouter from "./routes/user.js";
 import productRouter from "./routes/product.js";
 import cartRouter from "./routes/cart.js"
+import favoriteRouter from "./routes/favorite.js"
 import cookieParser from "cookie-parser";
 import cors from "cors"
 import { seedProducts } from "./services/productServices/seedProducts.js";
@@ -29,6 +30,7 @@ app.use(express.json());
 app.use("/users", userRouter);
 app.use("/products", productRouter);
 app.use("/cart", cartRouter);
+app.use("/favorite", favoriteRouter);
 
 
 seedProducts()
