4bacfb7c9f3b80be5f87e202998f8e2601cd1d00
Abdalla Elhagar <nanoelhgar@gmail.com>
2025-11-02 18:02:21 +0200

    Backend:
    - handled get user carts request

--- backend/routes/cart.js
+++ backend/routes/cart.js
@@ -4,6 +4,7 @@ import { addCartItem } from "../services/cartServices/addCartItem.js";
 import { getActiveCartForUser } from "../services/cartServices/getActiveCart.js";
 import { deleteCartItem } from "../services/cartServices/deleteCartItem.js";
 import { quantityControl } from "../services/cartServices/quantityControl.js";
+import { cartModel } from "../models/cart.js";
 const router = Router();
 
 router.get("/", async (req, res) => {
@@ -39,16 +40,41 @@ router.delete("/delete-cart-item", verifyJWT, async (req, res) => {
   }
 });
 
-router.put("/quantity-control", verifyJWT, async (req,res) => {
+router.put("/quantity-control", verifyJWT, async (req, res) => {
+  const userId = req.user._id;
+  const { control, productId } = req.body;
 
-  const userId = req.user._id
-  const { control, productId } = req.body
+  const respose = await quantityControl({ userId, control, productId });
 
-  const respose = await quantityControl({ userId, control, productId })
+  res.status(respose.statusCode).json(respose.data);
+});
 
-  res.status(respose.statusCode).json(respose.data)
+router.get("/complete-purchase-process", verifyJWT, async (req, res) => {
+  try {
+    const userId = req.user._id;
 
-})
+    const cart = await getActiveCartForUser({ userId });
 
+    cart.completed = true;
+
+    await cart.save()
+
+    res.status(200).json("complete-purchase-process");
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.get("/user-carts", verifyJWT, async (req, res) => {
+  try {
+    const userId = req.user._id;
+
+    const userCarts = await cartModel.find({userId})
+
+    res.status(200).json(userCarts);
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 export default router;
