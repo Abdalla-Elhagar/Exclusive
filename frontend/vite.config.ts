923e3f1e47ba2e9949aa4fb2008da5b517b43465
Abdalla Elhagar <nanoelhgar@gmail.com>
2025-11-06 10:00:47 +0200

    --

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from "@tailwindcss/vite";
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist',
-  },
-  server: {
-    port: 5173,
-  },
-  base: '/',
-})
+  plugins: [react(), tailwindcss()],
+  base: "/"
+});
