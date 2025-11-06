0a41e89b83c5a683a196e4c40df3843aa0a794df
Abdalla Elhagar <nanoelhgar@gmail.com>
2025-11-06 09:07:19 +0200

    Frontend: removed hash router and added BrowserRouter

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,16 +2,16 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { myStore } from "./store.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <HashRouter>
-      <Provider store={myStore}>
-        <App />
-      </Provider>
-    </HashRouter>
+    <BrowserRouter>
+  <Provider store={myStore}>
+    <App />
+  </Provider>
+</BrowserRouter>
   </StrictMode>
 );
